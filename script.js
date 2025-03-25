4246451e7427d2cf6a40a7f8cbd30faaf4d23b0e
zeelprajapati1 <zeelprajapati9876@gmail.com>
2025-03-26 04:03:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function Person(name, age) {
 }
 
 Person.prototype.greet = function() {
-    console.log(Hello, my name is ${this.name} and I am ${this.age} years old.);
+    console.log("Hello, my name is " + this.name + " and I am " + this.age + " years old.");
 };
 
 // Employee Constructor Function (inherits from Person)
@@ -21,7 +21,7 @@ Employee.prototype.constructor = Employee;
 
 // Add jobGreet method
 Employee.prototype.jobGreet = function() {
-    console.log(Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.);
+    console.log("Hello, my name is " + this.name + ", I am " + this.age + " years old, and my job title is " + this.jobTitle + ".");
 };
 
 // Do not change code below this line
